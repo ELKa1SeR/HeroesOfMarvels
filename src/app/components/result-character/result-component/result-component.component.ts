061140c1e53a30019acb6c7aec8e90c06a8110bf
FranciscoLiebanas <francisco.liebanas@nter.es>
2024-07-01 08:46:21 +0200

    "Finalisation of the Marvel logo route and results component".

--- src/app/components/result-character/result-component/result-component.component.ts
+++ src/app/components/result-character/result-component/result-component.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroResult } from '../../../interfaces/hero.interface';
+import { MarvelApiService } from '../../../services/marvel-api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-result-component',
-  template: `<span class="result">50 RESULTS</span>`,
+  template: `
+  <span class="result" *ngFor="let hero of heroes"></span>
+  <p class="results">{{ heroes.length }} Result</p>
+  `,
   styleUrls: ['./result-component.component.css']
 })
 export class ResultComponentComponent implements OnInit {
 
-  constructor() { }
+  heroes: HeroResult[] = [];
 
-  ngOnInit() {
+  constructor(private marvelApiService: MarvelApiService, private router: Router) {}
+
+
+  ngOnInit(): void {
+    this.marvelApiService.getHeroes().subscribe((heroes) => {
+      console.log('Heroes received:', heroes);  // Esto debería mostrar una lista de héroes
+      this.heroes = heroes.map(chat => ({
+        ...chat
+      }))
+    });
   }
 
 }
